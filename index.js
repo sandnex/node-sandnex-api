6b693a0db8c184f24dbacc4f7079881b376b538b
atcoolart <atcooltruewook@gmail.com>
2022-09-06 09:58:36 +0700

    ok
    update

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
-module.exports = function nthRt(n) {
-     return n ** (1/n)
+"use strict";
+const axios = require('axios');
+
+const base_url = 'https://api.sandnex.com/'
+
+
+let Sandnex = class Sandnex {
+
+    constructor() {
+        this.okd = 'easy';
+    }
+    options(options = {}) {
+        this.api_key = options.APIKEY;
+        this.api_secret = options.APISECRET;
+        // this.okd = 'aaaaaaaa';
+        // console.log(api_key);
+
+        return { status : true,api_key  : this.api_key ,api_secret : this.api_secret,balance : this.balance,buy : this.buy,pair : this.pair}
+    }
+
+    balance(symbol = null) {
+        return { status : true,message : null,symbol : symbol};
+    }
+
+    buy (pair,quantity,price) {
+        let result = parseFloat(quantity)*parseFloat(price);
+        return { status  : true,pair : pair,quantity : quantity,price : price,result : result}
+    }
+    
+    async pair () {
+        try {
+            const response = await axios.get(base_url+'/pairs');
+            return response.data;
+            
+        } catch (error) {
+            console.error(error);
+        }
+
+    }
+
 }
+
+
+module.exports = Sandnex;
---
+++ test-function/index.js
@@ -0,0 +1,20 @@
+const Sandnex = require('node-sandnex-api');
+
+const sandnex = new Sandnex().options({
+    APIKEY : '<test>',
+    APISECRET : '<test>'
+});
+
+// let quantity = 1, price = 0.069;
+// let buy_order = sandnex.buy("ETHUSDT", quantity, price);
+// console.log(buy_order)
+
+let pair = sandnex.pair();
+Promise.all([pair]).then((values) => {
+    console.log(values[0]);
+});
+
+
+
+// let balance = sandnex.balance();
+// console.log(balance)
